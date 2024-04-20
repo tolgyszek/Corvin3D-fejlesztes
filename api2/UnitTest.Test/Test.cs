01070150ea959f99cd85561cb216fcf7ec07929a
tolgyszeki <t.p.bence02@gmail.com>
2024-04-21 00:13:13 +0200

    email

---
+++ api2/UnitTest.Test/Test.cs
@@ -0,0 +1,26 @@
+﻿using api2;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Test
+{
+    public class Test
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new ugyfel();
+
+            // Act
+            var actualResult = ugyfel.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
